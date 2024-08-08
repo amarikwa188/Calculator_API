bbd7f32a3e13c8ab15205342bd10c09d3624c1f6
Amarikwa <amarikwa188@gmail.com>
2024-08-08 03:09:31 +0100

    implemented 'divide' and 'multiply' services

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 app: Flask = Flask(__name__)
 
 
-@app.route("/add", methods=["POST"])
+@app.route("/add/", methods=["POST"])
 def add():
     if request.method == "POST":
         try:
@@ -16,7 +16,7 @@ def add():
         except json.JSONDecodeError:
             return 'ERROR::Invalid Input -> input format: {"a":int, "b":int}'
 
-@app.route("/subtract", methods=["POST"])
+@app.route("/subtract/", methods=["POST"])
 def subtract():
     if request.method == "POST":
         try:
@@ -27,6 +27,31 @@ def subtract():
             return {'result': result}
         except json.JSONDecodeError:
             return 'ERROR::Invalid Input -> input format: {"a":int, "b":int}'
+        
+@app.route("/multiply/", methods=["POST"])
+def multiply():
+    if request.method == "POST":
+        try:
+            data: bytes = request.get_data()
+            json_data: dict = json.loads(data)
+
+            result: int = json_data["a"] * json_data["b"]
+            return {'result': result}
+        except json.JSONDecodeError:
+            return 'ERROR::Invalid Input -> input format: {"a":int, "b":int}'
+        
+@app.route("/divide/", method=["POST"])
+def divide():
+    if request.method == "POST":
+        try:
+            data: bytes = request.get_data()
+            json_data: dict = json.loads(data)
+
+            result: int = json_data["a"] / json_data["b"]
+            return {'result': result}
+        except json.JSONDecodeError:
+            return 'ERROR::Invalid Input -> input format: {"a":int, "b":int}'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
