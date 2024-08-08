ed43bf0538c92ffd2cc56a867ee39cc96bd75bdd
Amarikwa <amarikwa188@gmail.com>
2024-08-08 02:49:20 +0100

    implemented 'add' service

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+import json
 
+app: Flask = Flask(__name__)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    if request.method == "POST":
+        data: bytes = request.get_data()
+        json_data: dict = json.loads(data)
+        return {'result': sum(json_data.values())}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
