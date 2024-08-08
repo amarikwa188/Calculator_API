dedc604695463a88b180610d91f27bbd031f781b
Amarikwa <amarikwa188@gmail.com>
2024-08-08 03:02:17 +0100

    implemented 'subtract' service and error checking

--- app.py
+++ app.py
@@ -7,10 +7,26 @@ app: Flask = Flask(__name__)
 @app.route("/add", methods=["POST"])
 def add():
     if request.method == "POST":
-        data: bytes = request.get_data()
-        json_data: dict = json.loads(data)
-        return {'result': sum(json_data.values())}
+        try:
+            data: bytes = request.get_data()
+            json_data: dict = json.loads(data)
 
+            result: int = sum(json_data.values())
+            return {'result': result}
+        except json.JSONDecodeError:
+            return 'ERROR::Invalid Input -> input format: {"a":int, "b":int}'
+
+@app.route("/subtract", methods=["POST"])
+def subtract():
+    if request.method == "POST":
+        try:
+            data: bytes = request.get_data()
+            json_data: dict = json.loads(data)
+
+            result: int = json_data['a'] - json_data['b']
+            return {'result': result}
+        except json.JSONDecodeError:
+            return 'ERROR::Invalid Input -> input format: {"a":int, "b":int}'
 
 if __name__ == "__main__":
     app.run(debug=True)
